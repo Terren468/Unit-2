a291b95ef5aa055828685fce1285b4e550844a45
Terren468 <terrenh2@nycstudents.net>
2026-09-23 10:17:09 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,13 @@
+""" def divide(x,y):
+    return x/y 
+
+print( divide (2,2))
+if float:
+    print( "odd")
+elif float== False :
+    print ("even") """
+
+def bill (x,y):
+    return x+x*y
+
+print (bill(20, .5))
